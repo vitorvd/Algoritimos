4e5d256c40d728e79d29534dd49288f596b2e86e
Vitor Versiani <vitordversiani@gmail.com>
2021-03-23 10:46:19 -0300

    Update Bhaskara.cpp

--- Treinamento/Bhaskara.cpp
+++ Treinamento/Bhaskara.cpp
@@ -1,35 +1,35 @@
 //Entrada: a, b, c
-//SaÌda: x1 e x2
+//Sa√≠da: x1 e x2
 //Processamento: calcular bhaskara
 
-#include <stdio.h> // inclus„o da biblioteca que possuÌ as funÁıes "printf" e "scanf"
-#include <math.h> // inclus„o da biblioteca que ir· nos auxÌliar nos c·lculos matem·ticos
+#include <stdio.h> // inclus√£o da biblioteca que possu√≠ as fun√ß√µes "printf" e "scanf"
+#include <math.h> // inclus√£o da biblioteca que ir√° nos aux√≠liar nos c√°lculos matem√°ticos
 
-int main(){ // inÌcio do algoritmo: funÁ„o main
+int main(){ // in√≠cio do algoritmo: fun√ß√£o main
 
-	double a, b, c, delta, x1, x2; // declaraÁıes de vari·veis
+	double a, b, c, delta, x1, x2; // declara√ß√µes de vari√°veis
 	
-	printf("Qual o valor de A "); // impress„o do texto na tela do usu·rio
-	scanf("%lf", &a); // vinculaÁ„o do valor digitado pelo usu·rio ‡ respectiva vari·vel (no caso, valor de A)
+	printf("Qual o valor de A? "); // impress√£o do texto na tela do usu√°rio
+	scanf("%lf", &a); // vincula√ß√£o do valor digitado pelo usu√°rio √† respectiva vari√°vel (no caso, valor de A)
 
-	printf("Qual o valor de B "); // impress„o do texto na tela do usu·rio
-	scanf("%lf", &b);// vinculaÁ„o do valor digitado pelo usu·rio ‡ respectiva vari·vel (no caso, valor de B)
+	printf("Qual o valor de B? "); // impress√£o do texto na tela do usu√°rio
+	scanf("%lf", &b);// vincula√ß√£o do valor digitado pelo usu√°rio √† respectiva vari√°vel (no caso, valor de B)
 
-	printf("Qual o valor de C "); // impress„o do texto na tela do usu·rio
-	scanf("%lf", &c); // vinculaÁ„o do valor digitado pelo usu·rio ‡ respectiva vari·vel (no caso, valor de C)
+	printf("Qual o valor de C? "); // impress√£o do texto na tela do usu√°rio
+	scanf("%lf", &c); // vincula√ß√£o do valor digitado pelo usu√°rio √† respectiva vari√°vel (no caso, valor de C)
 	
-	delta = (pow(b, 2)) - 4 * a * c; // lineariazaÁ„o da fÛrmula de delta, com a inclsu„o da funÁ„o "pow" para calcular potÍncia
+	delta = (pow(b, 2)) - 4 * a * c; // lineariaza√ß√£o da f√≥rmula de delta, com a inclsu√£o da fun√ß√£o "pow" para calcular pot√™ncia
 
-	if((delta < 0) || (a == 0)) { //condicionamento das quebra de padrıes da bhaskara, ou seja: delta MENOR que ZERO ou A IGUAL a ZERO
-		printf("Bhaskara fora dos padrıes, n„o foi possÌvel calcular"); // impress„o do texto na tela do usu·rio
+	if((delta < 0) || (a == 0)) { //condicionamento das quebra de padr√µes da bhaskara, ou seja: delta MENOR que ZERO ou A IGUAL a ZERO
+		printf("Bhaskara fora dos padr√µes, n√£o foi poss√≠vel calcular!"); // impress√£o do texto na tela do usu√°rio
 	}else if(delta == 0){ // condicionamento de quando delta for IGUAL a ZERO, portanto, apenas um valor de X.
-		x1 = (-(b) + sqrt(delta)) / 2 * a; // lineariazaÁ„o da fÛrmula de bhaskara (calculando x1 e x2), com a inclus„o da funÁ„o "sqrt" para calcular raiz quadrade
-		printf("Os valores sao: \nX1: %.2lf \nX2: %.2lf", x1, x1); // impress„o do texto na tela do usu·rio, com os respectivos valores de X1 e X2.
+		x1 = (-(b) + sqrt(delta)) / 2 * a; // lineariaza√ß√£o da f√≥rmula de bhaskara (calculando x1 e x2), com a inclus√£o da fun√ß√£o "sqrt" para calcular raiz quadrade
+		printf("Os valores sao: \nX1: %.2lf \nX2: %.2lf", x1, x1); // impress√£o do texto na tela do usu√°rio, com os respectivos valores de X1 e X2.
 	}else{ // um bhaskara normal, com duas raizes e delta positivo
-		x1 = (-(b) + sqrt(delta)) / 2 * a; // lineariazaÁ„o da fÛrmula de bhaskara (calculando x1), com a inclus„o da funÁ„o "sqrt" para calcular raiz quadrade
-		x2 = (-(b) - sqrt(delta)) / 2 * a; // lineariazaÁ„o da fÛrmula de bhaskara (calculando x2), com a inclus„o da funÁ„o "sqrt" para calcular raiz quadrade
-		printf("Os valores sao: \nX1: %.2lf \nX2: %.2lf", x1, x2); // impress„o do texto na tela do usu·rio, com os respectivos valores de X1 e X2.
+		x1 = (-(b) + sqrt(delta)) / 2 * a; // lineariaza√ß√£o da f√≥rmula de bhaskara (calculando x1), com a inclus√£o da fun√ß√£o "sqrt" para calcular raiz quadrade
+		x2 = (-(b) - sqrt(delta)) / 2 * a; // lineariaza√ß√£o da f√≥rmula de bhaskara (calculando x2), com a inclus√£o da fun√ß√£o "sqrt" para calcular raiz quadrade
+		printf("Os valores sao: \nX1: %.2lf \nX2: %.2lf", x1, x2); // impress√£o do texto na tela do usu√°rio, com os respectivos valores de X1 e X2.
 	}
 	
-	return 0; // retorno padr„o da funÁ„o "main"
+	return 0; // retorno padr√£o da fun√ß√£o "main"
 } // fim do algoritmo 
