3826966ecfa6fa3729464aa0563d5664345ea7e5
vitorvd <vitordversiani@gmail.com>
2021-04-29 12:37:46 -0300

    fixing bug

--- Lista3/Ex11.cpp
+++ Lista3/Ex11.cpp
@@ -6,17 +6,17 @@
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 
-	int qntJuizes = 0, qntConcorrentes = 1, pontosTemp = 0, maiorPontuacao;
+	int qntJuizes = 0, qntConcorrentes = 3, pontosTemp = 0, maiorPontuacao;
 	
 	struct Concorrente {
 		char nome[40];
 		int pontos = 0;	
 	};
 	
-	Concorrente concorrentes[qntConcorrentes], concorrenteVencedor;
+	struct Concorrente concorrentes[qntConcorrentes], concorrenteVencedor;
 	
-	for(int concorrenteTemp = 0; concorrenteTemp <= qntConcorrentes; concorrenteTemp++){
-		printf("\nQual o nome do %dº concorrente? ", concorrenteTemp + 1);
+	for(int concorrenteTemp = 1; concorrenteTemp <= qntConcorrentes; concorrenteTemp++){
+		printf("\nQual o nome do %dº concorrente? ", concorrenteTemp);
 		scanf("%s", &concorrentes[concorrenteTemp].nome);
 		fflush(stdin);
 	}
@@ -25,33 +25,28 @@ int main(){
 	scanf("%d", &qntJuizes);
 	fflush(stdin);
 	
-	for(int concorrenteTemp = 0; concorrenteTemp <= qntConcorrentes; concorrenteTemp++){
-		concorrentes[concorrenteTemp].pontos = 0;
-		printf("\nNovo concorrente: %s - %d pts.\n", concorrentes[concorrenteTemp].nome, concorrentes[concorrenteTemp].pontos);
+	for(int concorrenteTemp = 1; concorrenteTemp <= qntConcorrentes; concorrenteTemp++){
 		for(int x = 1; x <= qntJuizes; x++){
-			//system("cls");
+			system("cls");
 			printf("\nQual a nota do %dº Juiz para o %s? ", x, concorrentes[concorrenteTemp].nome);
 			scanf("%d", &pontosTemp);
 			fflush(stdin);
-			concorrentes[concorrenteTemp].pontos = 0;
-			printf("pontosConcorrente: %d\n", concorrentes[concorrenteTemp].pontos);
-			concorrentes[concorrenteTemp].pontos = pontosTemp;
-			printf("pontosTemp: %d || pontosConcorrente: %d\n", pontosTemp, concorrentes[concorrenteTemp].pontos);
+			concorrentes[concorrenteTemp].pontos += pontosTemp;
 		}
-		if(concorrenteVencedor.pontos >= maiorPontuacao)
+		if(concorrentes[concorrenteTemp].pontos >= concorrenteVencedor.pontos)
 			concorrenteVencedor = concorrentes[concorrenteTemp];
 	}
 	
-	//system("cls");
+	system("cls");
 	
-	printf("\n\n===============================\n\n");
-	for(int concorrenteTemp = 0; concorrenteTemp <= qntConcorrentes; concorrenteTemp++){
+	printf("\n\n==========CONCORRENTES==========\n\n");
+	for(int concorrenteTemp = 1; concorrenteTemp <= qntConcorrentes; concorrenteTemp++){
 		printf("%s com %d pontos.\n", concorrentes[concorrenteTemp].nome, concorrentes[concorrenteTemp].pontos);
 	}
-	printf("\n===============================\n\n");
-	printf("\n\n===============================\n\n");
+	printf("\n==========CONCORRENTES==========\n\n");
+	printf("\n\n==========VENCEDOR===============\n\n");
 	printf("%s foi o vencedor com %d pontos.", concorrenteVencedor.nome, concorrenteVencedor.pontos);
-	printf("\n\n===============================\n\n");
+	printf("\n\n==========VENCEDOR===============\n\n");
 	
 	return 0;
 }
